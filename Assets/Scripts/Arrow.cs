d3a927b3dd0e3d98151dce67e63585d4df964a3a
Yegor <mareev.yegor@gmail.com>
2017-11-28 22:39:17 +0400

    Added physics and swipe controls

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,38 +4,22 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
-    public float moveSpeed;
-
-    private Vector2 oldPos, newPos;
-    private bool isMoving = false;
-    private float tmpTime = 0.0f;
+    Game game;
 
     void Start()
     {
-        oldPos = transform.position;
+        game = GameObject.Find("Game").GetComponent<Game>();
     }
 
     void Update()
     {
-        if (isMoving)
-        {
-            transform.position = Vector2.Lerp(oldPos, newPos, tmpTime);
-            tmpTime += Time.deltaTime / (Vector2.Distance(oldPos, newPos) / moveSpeed);
-            if (tmpTime >= 1)
-            {
-                isMoving = false;
-                tmpTime = 0.0f;
-            }
-        }
+
     }
 
-    public void MoveTo(Vector2 newPos)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!isMoving)
-        {
-            oldPos = transform.position;
-            this.newPos = newPos;
-            isMoving = true;
-        }
+        collision.otherRigidbody.simulated = false;
+        StartCoroutine(game.DelayedAction(game.SpawnArrow, 1));
+        StartCoroutine(game.DelayedAction(game.SpawnTarget, 1));
     }
 }
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,58 +9,60 @@ public class Game : MonoBehaviour
     public Vector2 archerPos;
     public Vector2 arrowArcherOffset;
     public float targetMinY;
+    public float minForce, maxForce;
 
     private GameObject arrow, target;
-    private Vector2 touchPos;
-    private bool isArrowDespawned = true;
-    private bool isTargetDespawned = true;
+    private Vector2 startTouchPos, endTouchPos, swipe;
 
     void Start()
     {
-
+        SpawnArrow();
+        SpawnTarget();
     }
 
     void Update()
     {
-        if (isArrowDespawned)
-        {
-            Destroy(arrow);
-
-            arrow = Instantiate(arrowPrefab, archerPos, transform.rotation);
-            arrow.transform.position += (Vector3)arrowArcherOffset;
-
-            isArrowDespawned = false;
-        }
-
-        if (isTargetDespawned)
-        {
-            Destroy(target);
-
-            SpawnTarget();
-
-            isTargetDespawned = false;
-        }
-
 #if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
         if (Input.touchCount > 0)
         {
-            touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-#else
-        if (Input.GetMouseButton(0))
-        {
-            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-#endif
-            if (touchPos.y > arrow.transform.position.y)
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
             {
-                arrow.GetComponent<Arrow>().MoveTo(touchPos);
+                startTouchPos = touch.position;
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                endTouchPos = touch.position;
+#else
+        { 
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log(arrow.transform.position);
+                startTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                endTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+#endif
+
+                swipe = endTouchPos - startTouchPos;
+
+                if (swipe.y < 0)
+                {
+                    var rigidBody = arrow.GetComponent<Rigidbody2D>();
+                    rigidBody.AddForce(-swipe * 300);
+                    rigidBody.simulated = true;
+                }
             }
         }
+    }
 
-        if (arrow.GetComponent<BoxCollider2D>().bounds.Intersects(target.GetComponent<CircleCollider2D>().bounds))
-        {
-            isArrowDespawned = true;
-            isTargetDespawned = true;
-        }
+    public void SpawnArrow()
+    {
+        Destroy(arrow);
+
+        arrow = Instantiate(arrowPrefab, archerPos, transform.rotation);
+        arrow.transform.position += (Vector3)arrowArcherOffset;
     }
 
     public void SpawnTarget()
@@ -68,6 +70,13 @@ public class Game : MonoBehaviour
         int x = UnityEngine.Random.Range(-2, 2);
         int y = UnityEngine.Random.Range(-2, 2);
         Vector2 targetPos = new Vector2(x, Math.Max(y, targetMinY));
+        Destroy(target);
         target = Instantiate(targetPrefab, targetPos, transform.rotation);
+    }
+
+    public IEnumerator DelayedAction(Action action, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        action();
     }
 }
